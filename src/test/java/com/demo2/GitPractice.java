863a7bb53bbcb85d39d7fea33cee90deffeb2733
ROHIT CHAWAN <chawanrohit863@gmail.com>
2025-09-19 21:47:58 +0530

    juct for practice

---
+++ src/test/java/com/demo2/GitPractice.java
@@ -0,0 +1,11 @@
+package com.demo2;
+
+import org.testng.annotations.Test;
+
+public class GitPractice {
+	 @Test
+public void sample() {
+	System.out.println("welcome to github practice session");
+}
+
+}
